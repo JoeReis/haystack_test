addcd66e04359662b9c50415cc94906d4650cf45
Joe Reis <josephreis@gmail.com>
2014-12-14 19:37:18 -0700

    create geo app. Create geo model

---
+++ geo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Geo(models.Model):
+    zip = models.CharField(max_length=10)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=10)
+    latitude = models.CharField(max_length=200)
+    longitude = models.CharField(max_length=200)
+    timezone = models.models.CharField(max_length=5)
+    dst = models.CharField(max_length=5)
