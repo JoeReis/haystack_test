f9f929e001931a92d5c15e339a1eab9ebb899077
Joe Reis <josephreis@gmail.com>
2015-01-12 20:26:09 -0700

    add custom template and form to geo

--- geo/models.py
+++ geo/models.py
@@ -1,6 +1,7 @@
 #from django.contrib.gis.geos import Pointet20144
 
 from django.db import models
+from django.contrib.gis.geos import Point
 
 
 class Geo(models.Model):
@@ -18,7 +19,7 @@ class Geo(models.Model):
     )
 
     distance = models.CharField(max_length=4, choices=DISTANCE_CHOICES, default=DISTANCE_5)
-    zip = models.CharField(max_length=10)
+    zip_code = models.CharField(max_length=10)
     city = models.CharField(max_length=200)
     state = models.CharField(max_length=10)
     latitude = models.FloatField()
@@ -26,8 +27,8 @@ class Geo(models.Model):
     timezone = models.CharField(max_length=5, blank=True, null=True)
     dst = models.CharField(max_length=5, blank=True, null=True)
 
-    def __str__(self):
-        return self.zip
-
     def get_location(self):
         return Point(self.longitude, self.latitude)
+
+    def __str__(self):
+        return self.zip_code
--- geo/search_indexes.py
+++ geo/search_indexes.py
@@ -4,7 +4,6 @@ from .models import Geo
 
 class GeoIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    zip = indexes.CharField(model_attr='zip')
     location = indexes.LocationField(model_attr='get_location')
 
     def get_model(self):
